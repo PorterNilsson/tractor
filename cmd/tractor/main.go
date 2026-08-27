2766e4dc338c868dd3aef9f41794d1606a484558
PorterNilsson <porter.nilsson@gmail.com>
2026-08-27 14:40:34 -0600

    Added main

--- cmd/tractor/main.go
+++ cmd/tractor/main.go
@@ -1,7 +1,67 @@
 package main
 
-import "github.com/PorterNilsson/tractor/internal/app"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-    app.Run()
+	if err := run(); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	addr := ":8080"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /health", handleHealth)
+
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
+	// Start server in background
+	errCh := make(chan error, 1)
+	go func() {
+		log.Printf("listening on %s", addr)
+		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			errCh <- err
+		}
+	}()
+
+	// Block until signal or server error
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err := <-errCh:
+		return fmt.Errorf("server error: %w", err)
+	case sig := <-sigCh:
+		log.Printf("received %s, shutting down", sig)
+	}
+
+	// Graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		return fmt.Errorf("shutdown: %w", err)
+	}
+
+	return nil
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "ok")
 }
